63631cf9d86012ca38f9a8dcd97e8bf305a7d888
jojoooo1 <chevalier.jonathan@live.fr>
2023-09-15 14:49:34 -0300

    wip

--- src/main/resources/db/migration/replication/V2.1.1__Create_replication.sql
+++ src/main/resources/db/migration/replication/V2.1.1__Create_replication.sql
@@ -5,9 +5,8 @@ GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "user";
 
 CREATE PUBLICATION dbz_publication FOR ALL TABLES;
 
--- CREATE TABLE debezium_signal (id VARCHAR(42) PRIMARY KEY, type VARCHAR(32) NOT NULL, data VARCHAR(2048) NULL);
 CREATE TABLE debezium_signal (id VARCHAR(42) PRIMARY KEY, type VARCHAR(32) NOT NULL, data VARCHAR(2048) NULL);
-CREATE TABLE debezium_heartbeat (message VARCHAR(32) NOT NULL, heartbeat timestamp NOT NULL);
+CREATE TABLE debezium_heartbeat (message VARCHAR(32) NOT NULL, heartbeat timestamp primary key NOT NULL);
 
 CREATE PUBLICATION my_publication FOR ALL TABLES WITH (publish = 'insert, update, delete, truncate');
 SELECT PG_CREATE_LOGICAL_REPLICATION_SLOT ('my_replication_slot', 'pgoutput');
