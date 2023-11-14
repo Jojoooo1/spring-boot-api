c347ea130074e7551e8fa9de45bd6c02d9362639
jojoooo1 <chevalier.jonathan@live.fr>
2023-11-14 18:40:39 -0300

    wip

--- src/main/java/com/mycompany/microservice/api/responses/shared/ApiErrorDetails.java
+++ src/main/java/com/mycompany/microservice/api/responses/shared/ApiErrorDetails.java
@@ -1,7 +1,10 @@
 package com.mycompany.microservice.api.responses.shared;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.io.Serializable;
 import lombok.Builder;
 
 @Builder
-public record ApiErrorDetails(String reason, String pointer) implements Serializable {}
+public record ApiErrorDetails(String reason, @JsonInclude(Include.NON_NULL) String pointer)
+    implements Serializable {}
