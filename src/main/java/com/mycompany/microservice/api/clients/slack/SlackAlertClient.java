1162924cd1d82833284c3605a1df76efa27cab7d
jojoooo1 <chevalier.jonathan@live.fr>
2023-11-22 19:30:07 -0300

    wip

--- src/main/java/com/mycompany/microservice/api/clients/slack/SlackAlertClient.java
+++ src/main/java/com/mycompany/microservice/api/clients/slack/SlackAlertClient.java
@@ -3,11 +3,9 @@ package com.mycompany.microservice.api.clients.slack;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-@Profile("!test")
 @Getter
 @Component
 public class SlackAlertClient extends BaseSlackClient {
